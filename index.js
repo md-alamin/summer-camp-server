7b4ad24bca017e4bab5942c8ed9ff0d24d063909
md-alamin <md.alamin.madrista@gmail.com>
2023-06-10 21:23:21 +0600

    add to cart done

--- index.js
+++ index.js
@@ -2,12 +2,34 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+	const authorization = req.headers.authorization;
+	if (!authorization) {
+		return res
+			.status(401)
+			.send({ error: true, message: 'unauthorized access' });
+	}
+
+	const token = authorization.split(' ')[1];
+
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+		if (err) {
+			return res
+				.status(401)
+				.send({ error: true, message: 'unauthorized access' });
+		}
+		req.decoded = decoded;
+		next();
+	});
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gt8d3ye.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -29,30 +51,74 @@ async function run() {
 			.db('summerCamp')
 			.collection('classCollection');
 
-		// const instructorCollection = client
-		// 	.db('summerCamp')
-		// 	.collection('instructorCollection');
-
 		const usersCollection = client
 			.db('summerCamp')
 			.collection('usersCollection');
 
-		// class api
-		app.get('/allClass', async (req, res) => {
-			const classes = await classCollection.find().toArray();
-			res.send(classes);
+		const cartCollection = client.db('summerCamp').collection('cartCollection');
+
+		// verify jwt
+		app.post('/jwt', (req, res) => {
+			const user = req.body;
+			const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+				expiresIn: '1h',
+			});
+			res.send({ token });
 		});
 
-		app.get('/sortClass', async (req, res) => {
+		const verifyStudent = async (req, res, next) => {
+			const email = req.decoded.email;
+			const query = { email: email };
+			const user = await usersCollection.findOne(query);
+			if (user?.role !== 'student') {
+				return res
+					.status(403)
+					.send({ error: true, message: 'forbidden message' });
+			}
+			next();
+		};
+
+		const verifyInstructor = async (req, res, next) => {
+			const email = req.decoded.email;
+			const query = { email: email };
+			const user = await usersCollection.findOne(query);
+			if (user?.role !== 'student') {
+				return res
+					.status(403)
+					.send({ error: true, message: 'forbidden message' });
+			}
+			next();
+		};
+
+		const verifyAdmin = async (req, res, next) => {
+			const email = req.decoded.email;
+			const query = { email: email };
+			const user = await usersCollection.findOne(query);
+			if (user?.role !== 'student') {
+				return res
+					.status(403)
+					.send({ error: true, message: 'forbidden message' });
+			}
+			next();
+		};
+
+		// class api
+		app.get('/allClass/:sort', async (req, res) => {
+			const classSort = req.params.sort;
 			const query = {};
 			const options = {
 				sort: { numberOfStudents: -1 },
 			};
-			const classes = await classCollection
-				.find(query, options)
-				.limit(6)
-				.toArray();
-			res.send(classes);
+			if (classSort === 'sort') {
+				const classes = await classCollection
+					.find(query, options)
+					.limit(6)
+					.toArray();
+				res.send(classes);
+			} else {
+				const classes = await classCollection.find().toArray();
+				res.send(classes);
+			}
 		});
 
 		// users api
@@ -73,6 +139,23 @@ async function run() {
 			res.send(students);
 		});
 
+		// cart api
+		app.get('/cart/:email', async (req, res) => {
+			const email = req.params.email;
+			if (!email) {
+				res.send();
+			}
+			const query = { email: email };
+			const result = await cartCollection.find(query).toArray();
+			res.send(result);
+		});
+
+		app.post('/cart', async (req, res) => {
+			const item = req.body;
+			const result = await cartCollection.insertOne(item);
+			res.send(result);
+		});
+
 		// instructor api
 		app.get('/users/instructor', async (req, res) => {
 			const query = { role: 'instructor' };
@@ -90,7 +173,6 @@ async function run() {
 		app.get('/class/:name', async (req, res) => {
 			const name = decodeURIComponent(req.params.name);
 			const query = { instructor: name };
-			console.log(query);
 			const instructorClass = await classCollection.find(query).toArray();
 			res.send(instructorClass);
 		});
